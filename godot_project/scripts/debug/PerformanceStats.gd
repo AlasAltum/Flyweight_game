cb2c7fa3dcb398d6b52d79fa8a20ce47a3233ed5
sebastianolmos <sebastianolmos@ug.uchile.cl>
2021-04-19 12:03:14 -0400

    ajustes de valores en las mecanicas

--- godot_project/scripts/debug/PerformanceStats.gd
+++ godot_project/scripts/debug/PerformanceStats.gd
@@ -1,6 +1,9 @@
 extends MarginContainer
 
+var memory_static
+
 func _physics_process(delta):
+	memory_static = stepify(Performance.get_monitor(Performance.MEMORY_STATIC) / (1024 * 1024), 0.01)
 	$HBoxContainer/VBoxContainer/Stat01.text = "FPS: " + str(Performance.get_monitor(Performance.TIME_FPS))
-	$HBoxContainer/VBoxContainer/Stat02.text = "Memory Static: " + str(Performance.get_monitor(Performance.MEMORY_STATIC / (1024*1024))) + " MB"
+	$HBoxContainer/VBoxContainer/Stat02.text = "Memory Static: " + str(memory_static) + " MB"
 	

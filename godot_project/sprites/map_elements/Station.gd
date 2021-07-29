2a2c6132c0dcc4063728048218631047117a90c2
AlasAltum <alonso.utreras@ug.uchile.cl>
2021-07-28 20:50:37 -0400

    arregla chips y agrega segunda cinemática

--- godot_project/sprites/map_elements/Station.gd
+++ godot_project/sprites/map_elements/Station.gd
@@ -1,9 +1,6 @@
 extends Area2D
 
-#onready var player = get_node("Player")
-export (Texture) var chip_texture;
-export (String) var new_chip_path;
-onready var floating_chip := $FloatingChip as Sprite
+
 onready var teleport_sound = $TeleportSound
 onready var animation = $StationTexture/AnimationPlayer
 onready var skill_menu = $SkillMenuLayer
@@ -14,7 +11,6 @@ var previous_motion
 
 func _ready() -> void:
 	# Solucion temporal para cerrar el menu luego de abrirlo con la tecla S
-	floating_chip.texture = chip_texture;
 	previous_motion = LevelManager.player.get_motion()
 	connect("body_entered", self, "on_body_entered")
 	connect("body_exited", self, "on_body_exited")
@@ -26,14 +22,6 @@ func on_body_entered(body: Node):
 	teleport_sound.play(0.0)
 	animation.play("Station")
 	get_tree().paused = true
-	
-	if first_time:
-		# Desaparece el chip y se encontrará entre las habilidades
-		LevelManager.first_time_skills.append(
-			$SkillMenuLayer.get_node(new_chip_path)
-		)
-		$FloatingChip/FloatingChipAnimation.play("Fade") # al final de esto aparece el menu
-		first_time = false
 
 
 func activate_skill_menu():
@@ -42,8 +30,3 @@ func activate_skill_menu():
 func _close_menu():
 	if previous_motion:
 		LevelManager.player.motion = previous_motion
-
-
-func _on_FloatingChipAnimation_animation_finished(anim_name):
-	if anim_name == "Fade":
-		$FloatingChip.visible = false
